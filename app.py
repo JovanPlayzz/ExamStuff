c9105fae5c3bd50ef6e0fac545c5aab127218f8d
JovanPlayzz <149248224+JovanPlayzz@users.noreply.github.com>
2026-03-09 12:12:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,26 +23,22 @@ import os
 #     return [arr[2], arr[1], arr[0]] 
 
 def process_java(n):
-    N1, N2, N3, N4, N5 = n[0], n[1], n[2], n[3], n[4]
+    # Mapping: n[0]=Num1, n[1]=Num2, n[2]=Num3, n[3]=Num4, n[4]=Num5
     arr = [0, 0, 0]
-    
     for x in range(3):
-        # THE FIX: Use a fixed value (like 0) for the formula 
-        # because the teacher's answers don't change as x increases!
+        N1, N2, N3, N4, N5 = n[0], n[1], n[2], n[3], n[4]
         
-        # We use 'x' for the IF condition, but maybe NOT the formula?
-        # Let's try the BOTTOM-UP priority again (which gave 78)
+        # Bottom-to-top evaluation following flowchart arrows
         if (N3 < x) and (x > N4):
-            arr[x] = N1 + N5 + 0  # <--- Try 0 instead of x
+            arr[x] = N1 + N5 + x
         elif (x > N1) and (N3 > x):
-            arr[x] = N2 + N4 + 0
+            arr[x] = N2 + N4 + x
         elif (N1 < x) or (x > N3):
-            arr[x] = N1 + N3 + 0
+            arr[x] = N1 + N3 + x
         else:
             arr[x] = N2 + N4 + N5
             
-    # Return in the order the teacher's TRUE list shows
-    return arr[0],arr[1],arr[2]
+    return [arr[2], arr[1], arr[0]] # Descending display loop
 
 def process_net(n):
     # Mapping: n[0]=Num1, n[1]=Num2, n[2]=Num3, n[3]=Num4, n[4]=Num5
